d0955ec079536b17119823b3b51aa0f30588f5a0
びくんびくん <big.stone.net@gmail.com>
2020-03-04 23:55:17 +0900

    add time entry struct

--- time_entry.go
+++ time_entry.go
@@ -1,1 +1,18 @@
 package redmine
+
+type TimeEntry struct {
+	Id        int     `json:"id"`
+	Project   IdName  `json:"project"`
+	Issue     Id      `json:"issue"`
+	User      IdName  `json:"user"`
+	Activity  IdName  `json:"activity"`
+	Hours     float32 `json:"hours"` //作業時間
+	Comments  string  `json:"comments"`
+	SpentOn   string  `json:"spent_on"` //作業日
+	CreatedOn string  `json:"created_on"`
+	UpdatedOn string  `json:"updated_on"`
+}
+
+func (c *Client) CreateTimeEntry() error {
+
+}
