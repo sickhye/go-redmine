bb7f867627ff5c0d7e5df4a70ad72c965cb3f6ec
bikun-bikun <big.stone.net@gmail.com>
2020-03-05 16:38:46 +0900

    add Create Time Entry

--- time_entry.go
+++ time_entry.go
@@ -1,5 +1,16 @@
 package redmine
 
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strings"
+)
+
+type timeEntryResult struct {
+	TimeEntry TimeEntry `json:"time_entry"`
+}
+
 type TimeEntry struct {
 	Id        int     `json:"id"`
 	Project   IdName  `json:"project"`
@@ -13,5 +24,53 @@ type TimeEntry struct {
 	UpdatedOn string  `json:"updated_on"`
 }
 
-func (c *Client) CreateTimeEntry(t TimeEntry) error {
+type TimeEntryRequest struct {
+	ProjectId  int     `json:"project_id,omitempty"`
+	IssueId    int     `json:"issue_id,omitempty"`
+	SpentOn    string  `json:"spent_on,omitempty"`
+	Hours      float32 `json:"hours,omitempty"`
+	ActivityId int     `json:"activity_id,omitempty"`
+	Comments   string  `json:"comments,omitempty"`
+}
+
+type timeEntryRequest struct {
+	TimeEntry TimeEntryRequest `json:"time_entry"`
+}
+
+func (c *Client) CreateTimeEntry(t TimeEntryRequest) (*TimeEntry, error) {
+	var item timeEntryRequest
+	item.TimeEntry = t
+	s, err := json.Marshal(item)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", c.endpoint+"/time_entries.json", strings.NewReader(string(s)))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("X-Redmine-API-Key", c.apikey)
+	req.Header.Set("Content-Type", "application/json")
+
+	res, err := c.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	decoder := json.NewDecoder(res.Body)
+	var r timeEntryResult
+	if res.StatusCode != 201 {
+		var er errorResult
+		err = decoder.Decode(&er)
+		if err == nil {
+			err = errors.New(strings.Join(er.Errors, "\n"))
+		}
+	} else {
+		err = decoder.Decode(&r)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return &r.TimeEntry, nil
 }
