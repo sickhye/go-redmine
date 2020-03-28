975e2806c302c6e1b2b97085eb96dbae173126b2
びくんびくん <big.stone.net@gmail.com>
2020-03-28 20:41:18 +0900

    add time entry filter

--- time_entry.go
+++ time_entry.go
@@ -3,6 +3,7 @@ package redmine
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
 	"strings"
 )
@@ -12,16 +13,16 @@ type timeEntryResult struct {
 }
 
 type TimeEntry struct {
-	Id        int     `json:"id"`
-	Project   *IdName `json:"project"`
-	Issue     *Id     `json:"issue"`
-	User      *IdName `json:"user"`
-	Activity  *IdName `json:"activity"`
-	Hours     float32 `json:"hours"` //作業時間
-	Comments  string  `json:"comments"`
-	SpentOn   string  `json:"spent_on"` //作業日
-	CreatedOn string  `json:"created_on"`
-	UpdatedOn string  `json:"updated_on"`
+	Id        int     `json:"id"`         // TimeEntry ID
+	Project   *IdName `json:"project"`    // Project
+	Issue     *Id     `json:"issue"`      // IssueID
+	User      *IdName `json:"user"`       // ユーザー
+	Activity  *IdName `json:"activity"`   // 作業分類
+	Hours     float32 `json:"hours"`      // 作業時間
+	Comments  string  `json:"comments"`   // コメント
+	SpentOn   string  `json:"spent_on"`   // 作業日
+	CreatedOn string  `json:"created_on"` // 作成日
+	UpdatedOn string  `json:"updated_on"` // 更新日
 }
 
 type TimeEntryRequest struct {
@@ -37,30 +38,30 @@ type timeEntryRequest struct {
 	TimeEntry *TimeEntryRequest `json:"time_entry"`
 }
 
+type TimeEntryFilter struct {
+	ProjectID string
+	UserID    string
+	From      string
+	To        string
+}
+
 func (c *Client) GetTimeEntries() ([]TimeEntry, error) {
-	res, err := c.Get(c.endpoint + "/time_entries.json?key=" + c.apikey)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-
-	decoder := json.NewDecoder(res.Body)
-	var t timeEntryResult
-	if res.StatusCode != http.StatusOK {
-		var er errorResult
-		err = decoder.Decode(&er)
-		if err == nil {
-			err = errors.New(strings.Join(er.Errors, "\n"))
-		}
-	} else {
-		err = decoder.Decode(&t)
-	}
+	timeEntries, err := getTimeEntries(c, c.endpoint+"/time_entries.json?key="+c.apikey)
 	if err != nil {
 		return nil, err
 	}
 
-	return t.TimeEntries, nil
+	return timeEntries, nil
 
+}
+
+func (c *Client) GetTimeEntriesByFilter(f *TimeEntryFilter) ([]TimeEntry, error) {
+	timeEntries, err := getTimeEntries(c, c.endpoint+"/time_entries.json?key="+c.apikey+getTimeEntryUrlQueryString(f))
+	if err != nil {
+		return nil, err
+	}
+
+	return timeEntries, nil
 }
 
 func (c *Client) CreateTimeEntry(t *TimeEntryRequest) ([]TimeEntry, error) {
@@ -99,4 +100,55 @@ func (c *Client) CreateTimeEntry(t *TimeEntryRequest) ([]TimeEntry, error) {
 		return nil, err
 	}
 	return r.TimeEntries, nil
+}
+
+func getTimeEntries(c *Client, url string) ([]TimeEntry, error) {
+	res, err := c.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	decoder := json.NewDecoder(res.Body)
+	var t timeEntryResult
+	if res.StatusCode != http.StatusOK {
+		var er errorResult
+		err = decoder.Decode(&er)
+		if err == nil {
+			err = errors.New(strings.Join(er.Errors, "\n"))
+		}
+	} else {
+		err = decoder.Decode(&t)
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return t.TimeEntries, nil
+}
+
+func getTimeEntryUrlQueryString(filter *TimeEntryFilter) string {
+	if filter == nil {
+		return ""
+	}
+
+	var q string
+	if filter.ProjectID != "" {
+		q = q + fmt.Sprintf("&project_id=%v", filter.ProjectID)
+	}
+
+	if filter.UserID != "" {
+		q = q + fmt.Sprintf("&user_id=%v", filter.UserID)
+	}
+
+	if filter.From != "" {
+		q = q + fmt.Sprintf("&from=%V", filter.From)
+	}
+
+	if filter.To != "" {
+		q = q + fmt.Sprintf("&to=%V", filter.To)
+	}
+
+	return q
+
 }
